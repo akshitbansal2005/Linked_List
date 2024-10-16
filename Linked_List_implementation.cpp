8cd47a15fa5b763f6d20aba537e5211a10f2703c
Akshit Bansal <155195875+akshitbansal2005@users.noreply.github.com>
2024-10-16 11:42:38 +0530

    Create Linked_List_implementation.cpp

---
+++ Linked_List_implementation.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+class Node{
+    public:
+    int value;
+    Node* next;
+Node(int data){
+        value=data;
+        next=NULL;
+    }
+};
+int main(){
+    Node* a=new Node(1);
+cout<<a->value<<" "<<a->next<<endl;
+}
+//Output
+/*1 0*/
